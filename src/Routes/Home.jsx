5b00cf948140c6260a5de4c8d8f65f0a1029ceb3
ShrabonKundu <shrabonkundu@gmail.com>
2025-03-01 02:02:40 +0600

    About  section added

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Navbar from '../Components/Navbar';
 import Hero from '../Components/Hero';
 import Skills from '../Components/Skills';
+import About from '../Components/About';
 
 const Home = () => {
     return (
         <div>
             <Navbar></Navbar>
             <Hero></Hero>
+            <About></About>
             <Skills></Skills>
         </div>
     );
